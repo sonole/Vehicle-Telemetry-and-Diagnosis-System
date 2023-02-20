3dc59d8c6d42202645020bf9ae0d525fbda9d979
Le <74705540+sonole@users.noreply.github.com>
2023-02-20 17:41:29 +0200

    Update StartTCPServer.php

--- server/app/Console/Commands/StartTCPServer.php
+++ server/app/Console/Commands/StartTCPServer.php
@@ -9,10 +9,13 @@ class StartTCPServer extends Command
 {
     protected $signature = 'tcp:start';
     protected $description = 'Start the TCP server';
+    protected $pidFile = 'tcp_server.pid';
 
     public function handle()
     {
         $tcpServer = new TCPServerService();
         $tcpServer->start();
+
+        file_put_contents($this->pidFile, getmypid());
     }
 }
