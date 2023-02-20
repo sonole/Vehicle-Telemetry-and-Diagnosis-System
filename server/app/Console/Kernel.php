3d0449f53598d00d2ae7f69aebed45526f656325
Le <74705540+sonole@users.noreply.github.com>
2023-02-20 17:43:28 +0200

    Update Kernel.php

--- server/app/Console/Kernel.php
+++ server/app/Console/Kernel.php
@@ -13,8 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
-          \App\Console\Commands\StartTCPServer::class,
+        \App\Console\Commands\StartTCPServer::class,
+        \App\Console\Commands\StopTCPServer::class,
     ];
 
     /**
