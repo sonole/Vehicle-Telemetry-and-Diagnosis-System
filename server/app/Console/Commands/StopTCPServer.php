078e011f1cd0af12ea5e48aa36e6c6115299a48b
Le <74705540+sonole@users.noreply.github.com>
2023-02-20 17:42:18 +0200

    Add files via upload

---
+++ server/app/Console/Commands/StopTCPServer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class StopStartTCPServer extends Command
+{
+    protected $signature = 'tcp:server:stop:starttcp';
+    protected $description = 'Stop the running instance of the StartTCPServer command';
+
+    public function handle()
+    {
+        $pidFile = storage_path('app/tcp_server.pid');
+
+        if (!file_exists($pidFile)) {
+            $this->error('No running instances of StartTCPServer found.');
+            return;
+        }
+
+        $pid = file_get_contents($pidFile);
+
+        if (!posix_kill($pid, 0)) {
+            $this->error('No running instances of StartTCPServer found.');
+            return;
+        }
+
+        exec("kill $pid");
+
+        $this->info('The running instance of StartTCPServer has been stopped.');
+    }
+}
