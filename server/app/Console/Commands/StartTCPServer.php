26f085caa50b0153be7101d9525def5b26d2ebcf
Le <74705540+sonole@users.noreply.github.com>
2023-02-24 22:33:08 +0200

    Update StartTCPServer.php

--- server/app/Console/Commands/StartTCPServer.php
+++ server/app/Console/Commands/StartTCPServer.php
@@ -9,16 +9,17 @@ class StartTCPServer extends Command
 {
     protected $signature = 'tcp:start';
     protected $description = 'Start the TCP server';
-    protected $pidFile = 'logs/tcp_server.pid';
 
     public function handle()
     {
+        $pidFile = storage_path('logs/tcp_server.pid');
+
         if (class_exists('App\Services\TCPServerService')) {
             $tcpServer = new TCPServerService();
-            file_put_contents(storage_path($this->pidFile), getmypid());
+            file_put_contents($pidFile, getmypid());
             $tcpServer->start();
         } else {
-            echo 'Error: TCPServerService class not found.'.PHP_EOL;
+            echo 'Error: TCPServerService class not found.'.PHP_EOL.PHP_EOL;
         }
     }
 }
